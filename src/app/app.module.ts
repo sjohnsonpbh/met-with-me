ffdf1a476cc7a97777fcfa95637ddb7fd3d3fb27
Emily Grace Lester <emhagerman5678@gmail.com>
2022-06-02 13:54:31 -0500

    NPM install

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
