2ce25abce8c82d79570292fde8937b612ec645c8
PRob-Solved <47791675+PRob-Solved@users.noreply.github.com>
2022-06-08 15:55:36 -0500

    Remove data service

---
+++ src/app/data.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { ArtObject } from './shared/artobject.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor() { }
+
+    // saveResultsToGlobalArray(any) {
+    // searchItemsDetails.map(ArtObject => {
+    //   const formattedArtObject = new ArtObject(
+    //     searchItemsDetails.title,
+    //     searchItemsDetails.artistDisplayName,
+    //     searchItemsDetails.objectDate,
+    //     searchItemsDetails.medium,
+    //     searchItemsDetails.linkResource,
+    //     searchItemsDetails.primaryImageSmall,
+    //     searchItemsDetails.rightsAndReproduction
+
+    //   );
+    //   this.SearchResults.push(formattedArtObject);
+    // });
+}
