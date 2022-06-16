35c8a18f9d60b451a75f517a24c425b39ad16898
Scott Johnson <47424009+sjohnsonpbh@users.noreply.github.com>
2022-06-16 08:58:30 -0500

    Navbar-My-Gallery - image on right

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
   collapsed = true;
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // this.isLoggedIn = this.authService.currUser()
+  }
+  // temporary simulate the authService
+  isLoggedIn = true;
 }
